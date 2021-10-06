f05c9db707b8c7a2809b855fd5c989310a2afc1e
Jutololla <julianangulop@gmail.com>
2021-10-05 20:58:48 -0500

    validaciones anadidas

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -26,21 +26,39 @@ public class Main {
     };
 
     static IntBinaryOperator div = (a, b) -> {
-        return IntStream.range(0, Math.abs(a) + 1)
+        if(b==0) throw new ArithmeticException("No es posible division entre 0");
+        int signo=1;
+        if(a>0&&b<0||b>0&&a<0){signo=-1;}
+        return mult.applyAsInt(signo,IntStream.range(0, Math.abs(a) + 1)
                 .reduce((acumulado, number) -> {
                             if (resta.applyAsInt(number,mult.applyAsInt(b,suma.applyAsInt(acumulado,1))) == 0) {
                                 acumulado=suma.applyAsInt(acumulado,1);
                             }
                             return acumulado;
                         }
-                ).getAsInt();
+                ).getAsInt());
     };
 
     public static void main(String[] args) {
         System.out.println(suma.applyAsInt(3, 2));
+        System.out.println(suma.applyAsInt(-3, 2));
+        System.out.println(suma.applyAsInt(3, -2));
+        System.out.println(suma.applyAsInt(-3, -2));
         System.out.println(resta.applyAsInt(3, 2));
+        System.out.println(resta.applyAsInt(-3, 2));
+        System.out.println(resta.applyAsInt(3, -2));
+        System.out.println(resta.applyAsInt(-3, -2));
         System.out.println(mult.applyAsInt(-5, -10));
-        System.out.println(div.applyAsInt(36, 5));
+        System.out.println(mult.applyAsInt(5, -10));
+        System.out.println(mult.applyAsInt(-5, 10));
+        System.out.println(mult.applyAsInt(5, 10));
+        System.out.println(mult.applyAsInt(0, 10));
+        System.out.println(mult.applyAsInt(0, 0));
+        System.out.println(div.applyAsInt(0, 15));
+        System.out.println(div.applyAsInt(-36, 15));
+        System.out.println(div.applyAsInt(-36, -15));
+        System.out.println(div.applyAsInt(36, -15));
+        System.out.println(div.applyAsInt(-36, 0));
     }
 
 }
